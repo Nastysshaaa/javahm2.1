ff0bb5aa9d213d820afe6a0cd2b94e4e34ba9a8d
Anastasiia <anastasia994@yandex.ru>
2023-02-16 21:34:13 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amountTicket = 10000; // стоимость билета в рублях
+        int mileOneAmount = 20; // стоимость мили в рублях
+
+        int miles; // объявление переменной для количества миль
+        if (amountTicket < 0) {
+            System.out.println("Стоимость билета не может быть отрицательной!"); // проверка неверных входных данных
+        } else {
+            miles = amountTicket / mileOneAmount; // подсчет кол-ва миль
+            System.out.println("Ваше количество миль: " + miles + "."); // вывод результата
+        }
     }
 }
